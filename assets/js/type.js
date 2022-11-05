3cd5f6c4e99c95ae4b9a0be65f03e9c9d9f2cd43
Dokza Equaristo <56497607+dokzaequaristo@users.noreply.github.com>
2022-11-05 13:41:50 +0700

    Add Source Code
    Upload Source Code

---
+++ assets/js/type.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.animate', {
+  strings: ['Apa Kabar?', 'Capek yaa?', 'Ngeluh Boleh Kok', 'Tapi Jangan Lama-Lama', 'Tetap Berproses yah', 'Jangan cuma berproses namun juga Berprogress', 'I Love You'],
+  backSpeed: 40,
+  typeSpeed: 80,
+  loop: true
+});
+
